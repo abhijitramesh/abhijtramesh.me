jjward25/portfolio2024

Remove next-seo and unneeded meta tags, add skip nav. (#269)

components/Container.js
@@ -1,19 +1,53 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
 import NextLink from 'next/link';
 
 import Footer from '@/components/Footer';
 
-export default function Container({ children }) {
+export default function Container(props) {
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
 
   // After mounting, we have access to the theme
   useEffect(() => setMounted(true), []);
 
+  const { children, ...customMeta } = props;
+  const router = useRouter();
+  const meta = {
+    title: 'Lee Robinson – Developer, writer, creator.',
+    description: `Front-end developer, JavaScript enthusiast, and course creator.`,
+    image: 'https://leerob.io/static/images/banner.png',
+    type: 'website',
+    ...customMeta
+  };
+
   return (
     <div className="bg-white dark:bg-black">
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta content={meta.description} name="description" />
+        <meta property="og:url" content={`https://leerob.io${router.asPath}`} />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:site_name" content="Lee Robinson" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@leeerob" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} />
+        {meta.date && (
+          <meta property="article:published_time" content={meta.date} />
+        )}
+      </Head>
       <nav className="sticky-nav flex justify-between items-center max-w-4xl w-full p-8 my-0 md:my-8 mx-auto bg-white dark:bg-black bg-opacity-60">
+        <a href="#skip" className="sr-only focus:not-sr-only">
+          Skip to content
+        </a>
         <button
           aria-label="Toggle Dark Mode"
           type="button"
@@ -63,7 +97,10 @@ export default function Container({ children }) {
           </NextLink>
         </div>
       </nav>
-      <main className="flex flex-col justify-center bg-white dark:bg-black px-8">
+      <main
+        id="skip"
+        className="flex flex-col justify-center bg-white dark:bg-black px-8"
+      >
         {children}
         <Footer />
       </main>

layouts/blog.js
@@ -4,7 +4,6 @@ import { parseISO, format } from 'date-fns';
 import Container from '@/components/Container';
 import Subscribe from '@/components/Subscribe';
 import ViewCounter from '@/components/ViewCounter';
-import BlogSeo from '@/components/BlogSeo';
 
 const editUrl = (slug) =>
   `https://github.com/leerob/leerob.io/edit/master/data/blog/${slug}.mdx`;
@@ -15,11 +14,13 @@ const discussUrl = (slug) =>
 
 export default function BlogLayout({ children, frontMatter }) {
   return (
-    <Container>
-      <BlogSeo
-        url={`https://leerob.io/blog/${frontMatter.slug}`}
-        {...frontMatter}
-      />
+    <Container
+      title={`${frontMatter.title} – Lee Robinson`}
+      description={frontMatter.summary}
+      image={`https://leerob.io${frontMatter.image}`}
+      date={new Date(frontMatter.publishedAt).toISOString()}
+      type="article"
+    >
       <article className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
           {frontMatter.title}

pages/dashboard.js
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import { NextSeo } from 'next-seo';
 
 import Analytics from '@/components/metrics/Analytics';
 import Buttondown from '@/components/metrics/Buttondown';
@@ -10,24 +9,12 @@ import Unsplash from '@/components/metrics/Unsplash';
 import YouTube from '@/components/metrics/Youtube';
 import TopTracks from '@/components/TopTracks';
 
-const url = 'https://leerob.io/dashboard';
-const title = 'Dashboard – Lee Robinson';
-const description =
-  'My personal dashboard, built with Next.js API routes deployed as serverless functions.';
-
 export default function Dashboard() {
   return (
-    <Container>
-      <NextSeo
-        title={title}
-        description={description}
-        canonical={url}
-        openGraph={{
-          url,
-          title,
-          description
-        }}
-      />
+    <Container
+      title="Dashboard – Lee Robinson"
+      description="My personal dashboard, built with Next.js API routes deployed as serverless functions."
+    >
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
           Dashboard

pages/newsletter.js
@@ -1,28 +1,14 @@
-import { NextSeo } from 'next-seo';
-
 import Container from '@/components/Container';
 import Subscribe from '@/components/Subscribe';
 import NewsletterLink from '@/components/NewsletterLink';
 import { getAllFilesFrontMatter } from '@/lib/mdx';
 
-const url = 'https://leerob.io/newsletter';
-const title = 'Newsletter – Lee Robinson';
-const description =
-  'Thoughts on the software industry, programming, tech, videography, music, and my personal life.';
-
 export default function Newsletter({ newsletters }) {
   return (
-    <Container>
-      <NextSeo
-        title={title}
-        description={description}
-        canonical={url}
-        openGraph={{
-          url,
-          title,
-          description
-        }}
-      />
+    <Container
+      title="Newsletter – Lee Robinson"
+      description="Thoughts on the software industry, programming, tech, videography, music, and my personal life."
+    >
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
           Newsletter

pages/snippets/index.js
@@ -1,23 +1,13 @@
-import { NextSeo } from 'next-seo';
-
 import Container from '@/components/Container';
 import FunctionCard from '@/components/FunctionCard';
 import { getAllFilesFrontMatter } from '@/lib/mdx';
 
-const url = 'https://leerob.io/snippets';
-const title = 'Code Snippets – Lee Robinson';
-
 export default function Snippets({ snippets }) {
   return (
-    <Container>
-      <NextSeo
-        title={title}
-        canonical={url}
-        openGraph={{
-          url,
-          title
-        }}
-      />
+    <Container
+      title="Code Snippets – Lee Robinson"
+      description="A collection of code snippets – including serverless functions, Node.js scripts, and CSS tricks."
+    >
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
           Code Snippets

abhijitramesh/abhijtramesh.me

components/Container.js
@@ -1,19 +1,53 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
 import NextLink from 'next/link';
 
 import Footer from '@/components/Footer';
 
-export default function Container({ children }) {
+export default function Container(props) {
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
 
   // After mounting, we have access to the theme
   useEffect(() => setMounted(true), []);
 
+  const { children, ...customMeta } = props;
+  const router = useRouter();
+  const meta = {
+    title: 'Abhijit Ramesh – Developer, Student, Bodybuilder',
+    description: `Machine Learning Engineer, Android Developer, and Startup Enthusiast`,
+    image: 'https://leerob.io/static/images/banner.jpg',
+    type: 'website',
+    ...customMeta
+  };
+
   return (
     <div className="bg-white dark:bg-black">
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta content={meta.description} name="description" />
+        <meta property="og:url" content={`https://leerob.io${router.asPath}`} />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:site_name" content="Lee Robinson" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@leeerob" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} />
+        {meta.date && (
+          <meta property="article:published_time" content={meta.date} />
+        )}
+      </Head>
       <nav className="sticky-nav flex justify-between items-center max-w-4xl w-full p-8 my-0 md:my-8 mx-auto bg-white dark:bg-black bg-opacity-60">
+        <a href="#skip" className="sr-only focus:not-sr-only">
+          Skip to content
+        </a>
         <button
           aria-label="Toggle Dark Mode"
           type="button"
@@ -63,7 +97,10 @@ export default function Container({ children }) {
           </NextLink>
         </div>
       </nav>
-      <main className="flex flex-col justify-center bg-white dark:bg-black px-8">
+      <main
+        id="skip"
+        className="flex flex-col justify-center bg-white dark:bg-black px-8"
+      >
         {children}
         <Footer />
       </main>

layouts/blog.js
@@ -4,7 +4,6 @@ import { parseISO, format } from 'date-fns';
 import Container from '@/components/Container';
 import Subscribe from '@/components/Subscribe';
 import ViewCounter from '@/components/ViewCounter';
-import BlogSeo from '@/components/BlogSeo';
 
 const editUrl = (slug) =>
   `https://github.com/abhijitramesh/abhijitramesh.me/edit/master/data/blog/${slug}.mdx`;
@@ -15,11 +14,14 @@ const discussUrl = (slug) =>
 
 export default function BlogLayout({ children, frontMatter }) {
   return (
-    <Container>
-      <BlogSeo
-        url={`https://abhijitramesh.me/blog/${frontMatter.slug}`}
-        {...frontMatter}
-      />
+
+    <Container
+      title={`${frontMatter.title} – Lee Robinson`}
+      description={frontMatter.summary}
+      image={`https://abhijitramesh.me${frontMatter.image}`}
+      date={new Date(frontMatter.publishedAt).toISOString()}
+      type="article"
+    >
       <article className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
           {frontMatter.title}

pages/dashboard.js
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import { NextSeo } from 'next-seo';
 
 import Analytics from '@/components/metrics/Analytics';
 import Buttondown from '@/components/metrics/Buttondown';
@@ -8,24 +7,13 @@ import GitHub from '@/components/metrics/Github';
 import YouTube from '@/components/metrics/Youtube';
 import TopTracks from '@/components/TopTracks';
 
-const url = 'https://abhijitramesh.me/dashboard';
-const title = 'Dashboard – Abhijit Ramesh';
-const description =
-  'My personal dashboard, built with Next.js API routes deployed as serverless functions.';
 
 export default function Dashboard() {
   return (
-    <Container>
-      <NextSeo
-        title={title}
-        description={description}
-        canonical={url}
-        openGraph={{
-          url,
-          title,
-          description
-        }}
-      />
+    <Container
+      title="Dashboard – Abhijit Ramesh"
+      description="My personal dashboard"
+    >
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
           Dashboard

pages/newsletter.js
@@ -1,28 +1,15 @@
-import { NextSeo } from 'next-seo';
-
 import Container from '@/components/Container';
 import Subscribe from '@/components/Subscribe';
 import NewsletterLink from '@/components/NewsletterLink';
 import { getAllFilesFrontMatter } from '@/lib/mdx';
 
-const url = 'https://abhijitramesh.me/newsletter';
-const title = 'Newsletter – Abhijit Ramesh';
-const description =
-  'Thoughts on the software industry, programming, tech, videography, music, and my personal life.';
 
 export default function Newsletter({ newsletters }) {
   return (
-    <Container>
-      <NextSeo
-        title={title}
-        description={description}
-        canonical={url}
-        openGraph={{
-          url,
-          title,
-          description
-        }}
-      />
+    <Container
+      title="Newsletter – Abhijit Ramesh"
+      description="Monthy Newsletter on Machine Learning and more...."
+    >
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
           Newsletter

pages/snippets/index.js
@@ -1,32 +1,21 @@
-import { NextSeo } from 'next-seo';
-
 import Container from '@/components/Container';
 import FunctionCard from '@/components/FunctionCard';
 import { getAllFilesFrontMatter } from '@/lib/mdx';
 
-const url = 'https://abhijitramesh.me/snippets';
-const title = 'Code Snippets – Abhijit Ramesh';
 
 export default function Snippets({ snippets }) {
   return (
-    <Container>
-      <NextSeo
-        title={title}
-        canonical={url}
-        openGraph={{
-          url,
-          title
-        }}
-      />
+    <Container
+      title="Code Snippets – Abhijit Ramesh"
+      description="A collection of code snippets "
+    >
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
           Code Snippets
         </h1>
         <p className="text-gray-600 dark:text-gray-400 mb-4">
           These are a collection of code snippets I've used in the past and
-          saved. Some are Serverless Functions, which include set up
-          instructions. Others are anything from random CSS snippets to Node.js
-          scripts.
+          saved.
         </p>
         <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 my-2 w-full mt-4">
           {snippets.map((snippet) => (