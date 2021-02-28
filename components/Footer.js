53c225b079a00a89c2a5df580a728cc49094f761
Abhijit Ramesh <abhijitramesh2k@gmail.com>
2021-03-01 00:42:14 +0530

    Style Footer, Now Playing

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import NowPlaying from '@/components/NowPlaying';
 
 const ExternalLink = ({ href, children }) => (
   <a
-    className="text-sm text-gray-500 hover:text-gray-600 transition"
+    className="text-gray-500 hover:text-gray-600 transition"
     target="_blank"
     rel="noopener noreferrer"
     href={href}
@@ -15,106 +15,49 @@ const ExternalLink = ({ href, children }) => (
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col items-center mb-8">
+    <footer className="flex flex-col justify-center items-start max-w-2xl mx-auto w-full mb-8">
+      <hr className="w-full border-1 border-gray-200 dark:border-gray-800 mb-8" />
       <NowPlaying />
-      <div className="flex space-x-4 mb-4">
-        <ExternalLink href="https://twitter.com/abhijitramesh2k">
-          <span className="sr-only">Twitter</span>
-          <svg className="h-5 w-5" viewBox="0 0 24 24">
-            <g
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
-            </g>
-          </svg>
-        </ExternalLink>
-        <ExternalLink href="https://github.com/abhijitramesh">
-          <span className="sr-only">Github</span>
-          <svg className="h-5 w-5" viewBox="0 0 24 24">
-            <g
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
-            </g>
-          </svg>
-        </ExternalLink>
-        <ExternalLink href="https://www.linkedin.com/in/abhijit-ramesh-b583ab185/">
-          <span className="sr-only">LinkedIn</span>
-          <svg className="h-5 w-5" viewBox="0 0 24 24">
-            <g
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
-              <rect x="2" y="9" width="4" height="12" />
-              <circle cx="4" cy="4" r="2" />
-            </g>
-          </svg>
-        </ExternalLink>
-        <ExternalLink href="https://www.youtube.com/channel/UCHB7v9t-ZOYE0ssOE5az04A">
-          <span className="sr-only">YouTube</span>
-          <svg className="h-5 w-5" viewBox="0 0 24 24">
-            <g
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M22.54 6.42a2.78 2.78 0 00-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 00-1.94 2A29 29 0 001 11.75a29 29 0 00.46 5.33A2.78 2.78 0 003.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 001.94-2 29 29 0 00.46-5.25 29 29 0 00-.46-5.33z" />
-              <path d="M9.75 15.02l5.75-3.27-5.75-3.27v6.54z" />
-            </g>
-          </svg>
-        </ExternalLink>
-        <ExternalLink href="mailto:abhijitramesh2k@gmail.com">
-          <span className="sr-only">Email</span>
-          <svg className="h-5 w-5" viewBox="0 0 24 24">
-            <g
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
-              <polyline points="22,6 12,13 2,6" />
-            </g>
-          </svg>
-        </ExternalLink>
-      </div>
-      <div className="space-x-3">
-        <Link href="/uses">
-          <a className="text-sm text-gray-500 hover:text-gray-600">/uses</a>
-        </Link>
-        {/*<Link href="/newsletter">*/}
-        {/*  <a className="text-sm text-gray-500 hover:text-gray-600">*/}
-        {/*    /newsletter*/}
-        {/*  </a>*/}
-        {/*</Link>*/}
-        {/* <Link href="/snippets">*/}
-        {/*  <a className="text-sm text-gray-500 hover:text-gray-600">/snippets</a>*/}
-        {/*</Link>*/}
-        {/*<Link href="/tweets">*/}
-        {/*  <a className="text-sm text-gray-500 hover:text-gray-600">/tweets</a>*/}
-        {/*</Link> */}
-      </div>
-      <div>
-      <Link href="https://github.com/leerob/leerob.io">
-          <a className="text-sm text-gray-500 hover:text-gray-600">
-            This website is build from leerob.io, If you like what you see click here.
-          </a>
-        </Link>
+      <div className="w-full max-w-2xl grid grid-cols-1 gap-4 pb-16 sm:grid-cols-3">
+        <div className="flex flex-col space-y-4">
+          <Link href="/">
+            <a className="text-gray-500 hover:text-gray-600 transition">Home</a>
+          </Link>
+          <Link href="/blog">
+            <a className="text-gray-500 hover:text-gray-600 transition">Blog</a>
+          </Link>
+          <Link href="/uses">
+            <a className="text-gray-500 hover:text-gray-600 transition">Uses</a>
+          </Link>
+          {/*<Link href="/about">*/}
+          {/*  <a className="text-gray-500 hover:text-gray-600 transition">*/}
+          {/*    About*/}
+          {/*  </a>*/}
+          {/*</Link>*/}
+          {/*<Link href="/newsletter">*/}
+          {/*  <a className="text-gray-500 hover:text-gray-600 transition">*/}
+          {/*    Newsletter*/}
+          {/*  </a>*/}
+          {/*</Link>*/}
+        </div>
+        <div className="flex flex-col space-y-4">
+          <ExternalLink href="https://www.linkedin.com/in/abhijit-ramesh-b583ab185/">
+            Linkedin
+          </ExternalLink>
+          <ExternalLink href="https://github.com/abhijitramesh">GitHub</ExternalLink>
+          <ExternalLink href="https://gitlab.com/abhijitramesh">GitLab</ExternalLink>
+        </div>
+        <div className="flex flex-col space-y-4">
+          <ExternalLink href="https://twitter.com/abhijitramesh2k">
+            Twitter
+          </ExternalLink>
+          <ExternalLink href="https://www.youtube.com/channel/UCHB7v9t-ZOYE0ssOE5az04A">
+            YouTube
+          </ExternalLink>
+          <ExternalLink href="https://www.instagram.com/the_abhijit_ramesh/">
+            Instagram
+          </ExternalLink>
+        </div>
       </div>
     </footer>
   );
