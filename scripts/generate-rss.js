jjward25/portfolio2024

Add RSS feed. (#270)

scripts/generate-rss.js
@@ -1,0 +1,34 @@
+const { promises: fs } = require('fs');
+const path = require('path');
+const RSS = require('rss');
+const matter = require('gray-matter');
+
+async function generate() {
+  const feed = new RSS({
+    title: 'Lee Robinson',
+    site_url: 'https://leerob.io',
+    feed_url: 'https://leerob.io/feed.xml'
+  });
+
+  const posts = await fs.readdir(path.join(__dirname, '..', 'data', 'blog'));
+
+  await Promise.all(
+    posts.map(async (name) => {
+      const content = await fs.readFile(
+        path.join(__dirname, '..', 'data', 'blog', name)
+      );
+      const frontmatter = matter(content);
+
+      feed.item({
+        title: frontmatter.data.title,
+        url: 'https://leerob.io/blog/' + name.replace(/\.mdx?/, ''),
+        date: frontmatter.data.publishedAt,
+        description: frontmatter.data.summary
+      });
+    })
+  );
+
+  await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }));
+}
+
+generate();

abhijitramesh/abhijtramesh.me

scripts/generate-rss.js
@@ -1,0 +1,34 @@
+const { promises: fs } = require('fs');
+const path = require('path');
+const RSS = require('rss');
+const matter = require('gray-matter');
+
+async function generate() {
+  const feed = new RSS({
+    title: 'Abhijit Ramesh',
+    site_url: 'https://abhijitramesh.me',
+    feed_url: 'https://abhijitramesh.me/feed.xml'
+  });
+
+  const posts = await fs.readdir(path.join(__dirname, '..', 'data', 'blog'));
+
+  await Promise.all(
+    posts.map(async (name) => {
+      const content = await fs.readFile(
+        path.join(__dirname, '..', 'data', 'blog', name)
+      );
+      const frontmatter = matter(content);
+
+      feed.item({
+        title: frontmatter.data.title,
+        url: 'https://leerob.io/blog/' + name.replace(/\.mdx?/, ''),
+        date: frontmatter.data.publishedAt,
+        description: frontmatter.data.summary
+      });
+    })
+  );
+
+  await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }));
+}
+
+generate();